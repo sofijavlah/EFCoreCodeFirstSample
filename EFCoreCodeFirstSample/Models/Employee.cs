7457da54c456b8614d3381def7140921a7c470a9
sofijavlah <vlahovic.sofija@gmail.com>
2019-02-13 12:58:44 +0100

    Initial commit

---
+++ EFCoreCodeFirstSample/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreCodeFirstSample.Models
+{
+    public class Employee
+    {
+        //We have decorated the EmployeeId property with Key and DatabaseGenerated attributes.
+        //We did this because we will be converting this class into a database table
+        //and the column EmployeeId will serve as our primary key
+        //with the auto-incremented identity.
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long EmployeeId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ EFCoreCodeFirstSample/Models/EmployeeContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace EFCoreCodeFirstSample.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+        }
+
+
+        public EmployeeContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
